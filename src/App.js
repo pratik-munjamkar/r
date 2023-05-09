7961d19f6b50b9d745beb99c2df91b51e04ff939
pratik0626 <89349824+pratik-munjamkar@users.noreply.github.com>
2023-05-10 05:22:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <div className="container">
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
